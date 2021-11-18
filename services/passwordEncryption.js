305767d2f786f15e763f03acad62a80c31954590
YehonatanKadosh <jacojacoj3@gmail.com>
2021-11-18 19:01:44 +0200

    encrypt and validate passwords

---
+++ services/passwordEncryption.js
@@ -0,0 +1,13 @@
+const bcrypt = require("bcrypt");
+let salt = undefined;
+
+const encryptPassword = async (password) => {
+  if (salt === undefined) salt = await bcrypt.genSalt(10);
+  return await bcrypt.hash(password, salt);
+};
+
+// returns boolean value (is it Valid)
+const validatePassword = async (enteredPassword, savedPassword) =>
+  await bcrypt.compare(enteredPassword, savedPassword);
+
+module.exports = { encryptPassword, validatePassword };
