2db591c8ce795c0697ea0bf493462664ab5682f8
YehonatanKadosh <jacojacoj3@gmail.com>
2021-11-18 18:50:52 +0200

    adding generic crud requests

---
+++ services/mongoose/requestHandlers/generiCRUD.js
@@ -0,0 +1,11 @@
+const genericGet = async (_id, mongoModel) => await mongoModel.findOne({ _id });
+
+const genericUpdate = async (_id, updateFilter, mongoModel) =>
+  await mongoModel.updateOne({ _id }, updateFilter);
+
+const genericCreate = async (model_arguments, validator, mongoModel) => {
+  await validator.validateAsync(model_arguments);
+  res(await (await new mongoModel(model_arguments)).save());
+};
+
+module.exports = { genericGet, genericUpdate, genericCreate };
