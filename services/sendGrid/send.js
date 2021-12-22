6fb1d193bc8a5bb9f498faf68527d53e5f33c3f5
YehonatanKadosh <jacojacoj3@gmail.com>
2021-12-22 19:02:19 +0200

    removed mailing send loggigng

--- services/sendGrid/send.js
+++ services/sendGrid/send.js
@@ -2,14 +2,9 @@ const sgMail = require("@sendgrid/mail");
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const sendEmail = (msg) => {
-  sgMail
-    .send(msg)
-    .then((a) => {
-      console.log("Email sent");
-    })
-    .catch((error) => {
-      console.error(error);
-    });
+  sgMail.send(msg).catch((error) => {
+    console.error(error);
+  });
 };
 
 module.exports = sendEmail;
